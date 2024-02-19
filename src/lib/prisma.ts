e9d7336eb13911d7b7c63f5b3e0d70aceb1d7371
frsErley <frsleeh@gmail.com>
2024-02-19 15:10:22 -0300

    feat: add auth handler to header component

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,17 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@prisma/client";
 
 declare global {
   var cachedPrisma: PrismaClient;
 }
 
 let prisma: PrismaClient;
-
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
   if (!global.cachedPrisma) {
     global.cachedPrisma = new PrismaClient();
   }
-  prisma - global.cachedPrisma;
+  prisma = global.cachedPrisma;
 }
 
 export const prismaClient = prisma;
