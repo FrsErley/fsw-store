4876813939122497c30c02fe75a4be013d3b7bb3
frsErley <frsleeh@gmail.com>
2024-03-04 14:02:03 -0300

    feat: add mouse section to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -22,6 +22,14 @@ export default async function Home() {
     },
   });
 
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "mouses",
+      },
+    },
+  });
+
   return (
     <div className="flex flex-col gap-8">
       <PromoBanner src="/banner-home-01.png" alt="Até 55% de desconto esse mês!" />
@@ -42,11 +50,12 @@ export default async function Home() {
         <ProductList products={keyboards} />
       </div>
 
+      <PromoBanner src="/banner-home-03.png" alt="Até 55% de desconto em mouses!" />
+
       <div>
-        <SectionTitle>Ofertas!</SectionTitle>
-        <ProductList products={deals} />
+        <SectionTitle>Mouses</SectionTitle>
+        <ProductList products={mouses} />
       </div>
-      <PromoBanner src="/banner-home-03.png" alt="" />
     </div>
   );
 }
