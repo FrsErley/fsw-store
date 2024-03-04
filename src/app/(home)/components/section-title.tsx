db9a34c27ca8daf6a503f3fa1600458a3cc94211
frsErley <frsleeh@gmail.com>
2024-03-04 13:58:37 -0300

    feat: add third promo banner to home page

--- src/app/(home)/components/section-title.tsx
+++ src/app/(home)/components/section-title.tsx
@@ -1,7 +1,11 @@
 import { ComponentProps } from "react";
 
 const SectionTitle = ({ children, ...props }: ComponentProps<"p">) => {
-  return <p className="font-bold uppercase pl-5 mb-2">(...props){children}</p>;
+  return (
+    <p className="font-bold uppercase pl-5 mb-2" {...props}>
+      {children}
+    </p>
+  );
 };
 
 export default SectionTitle;
